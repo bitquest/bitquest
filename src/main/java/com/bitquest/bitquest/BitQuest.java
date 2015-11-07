891a53ee2b796d5dbb6bb58f323be830364692e8
explodi <phaser@tubatuba.net>
2015-11-07 16:56:26 -0300

    BlockEvents initialization error

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -19,7 +19,7 @@ public class BitQuest extends JavaPlugin {
     public void onEnable() {
         log.info("BitQuest starting...");
         // registers listener classes
-        getServer().getPluginManager().registerEvents(new BlockEvents(),this);
+        getServer().getPluginManager().registerEvents(new BlockEvents(this),this);
         // loads config file. If it doesn't exist, creates it.
         // get plugin config
         getDataFolder().mkdir();
