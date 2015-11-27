cbfd517a40f87fde980182ee79134767727e3d0d
explodi <phaser@tubatuba.net>
2015-11-27 01:17:48 -0300

    place banner listener

--- src/main/java/com/bitquest/bitquest/BlockEvents.java
+++ src/main/java/com/bitquest/bitquest/BlockEvents.java
@@ -9,7 +9,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -51,13 +53,56 @@ public class BlockEvents implements Listener {
     }
 	@EventHandler
 	void onBlockPlace(BlockPlaceEvent event) {
-		// If block is bedrock, cancel the event
-		if(event.getBlock().getType().equals(Material.BEDROCK)) {
+		// set clan
+		// first, we check if the player has permission to build
+		if (bitQuest.canBuild(event.getBlock().getLocation(), event.getPlayer()) == false) {
+			JsonObject area = bitQuest.areaForLocation(event.getBlock().getLocation());
+			if(area.get("name") != null) {
+				bitQuest.error(event.getPlayer(), "You may not break blocks in " + area.get("name").getAsString() + "!");
+			} else {
+				bitQuest.error(event.getPlayer(), "You may not break blocks here!");
+			}
+			event.setCancelled(true);
+		} else if (event.getBlock().getType() == Material.STANDING_BANNER) {
+
+			if (bitQuest.areaForLocation(event.getBlock().getLocation()) != null) {
+				// Banner banner=(Banner)e.getBlock();
+				Block belowBlock = event.getBlock().getRelative(0, -1, 0);
+				Sign sign = null;
+				if (belowBlock.getRelative(BlockFace.EAST).getType() == Material.WALL_SIGN) {
+					sign = (Sign) belowBlock.getRelative(BlockFace.EAST).getState();
+				}
+				if (belowBlock.getRelative(BlockFace.WEST).getType().equals(Material.WALL_SIGN)) {
+					sign = (Sign) belowBlock.getRelative(BlockFace.WEST).getState();
+				}
+				if (belowBlock.getRelative(BlockFace.NORTH).getType().equals(Material.WALL_SIGN)) {
+					sign = (Sign) belowBlock.getRelative(BlockFace.NORTH).getState();
+				}
+				if (belowBlock.getRelative(BlockFace.SOUTH).getType().equals(Material.WALL_SIGN)) {
+					sign = (Sign) belowBlock.getRelative(BlockFace.SOUTH).getState();
+				}
+				if (sign != null) {
+					String tag = sign.getLine(0).toLowerCase();
+					if (tag.length() > 12) {
+						event.getPlayer().sendMessage("Clan names are limited to a maximum of 12 characters.");
+					} else if (tag.length() > 0) {
+						// TODO: Find out if the clan name already exists and check player is invited
+						// TODO: Add a clan property to area key
+						// TODO: Add a clan property to user data
+					} else {
+						event.getPlayer().sendMessage("Please write the name of the clan on the first line of the sign.");
+					}
+
+
+				}
+			} else {
+				event.getPlayer().sendMessage("You can only place banners in your home land.");
+				event.setCancelled(true);
+			}
+		} else if(event.getBlock().getType().equals(Material.BEDROCK)) {
 			bitQuest.error(event.getPlayer(), "Placing bedrock is not allowed!");
     		event.setCancelled(true);
-		}
-        // If block is bed, attempt to claim area for player
-        if (event.getBlock().getType() == Material.BED_BLOCK) {
+		} else if (event.getBlock().getType() == Material.BED_BLOCK) {
             List<String> areas = bitQuest.REDIS.lrange("areas", 0, -1);
 
             for (String areaJSON : areas) {
@@ -80,19 +125,8 @@ public class BlockEvents implements Listener {
             } else {
                 bitQuest.error(event.getPlayer(), "Plot claim error");
             }
-        } else {
-
-        	if (bitQuest.canBuild(event.getBlock().getLocation(), event.getPlayer()) == false) {
-        		JsonObject area = bitQuest.areaForLocation(event.getBlock().getLocation());
-        		if(area.get("name") != null) {
-        			bitQuest.error(event.getPlayer(), "You may not break blocks in " + area.get("name").getAsString() + "!");
-        		} else {
-        			bitQuest.error(event.getPlayer(), "You may not break blocks here!");
-        		}
-                event.setCancelled(true);
-            }
-    		
         }
+
         
 	}
     // Make a sweet block regeneration ^_^
