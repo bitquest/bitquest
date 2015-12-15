7f4cb6b234b1c18b628d45da757296a1a5c1747e
explodi <phaser@tubatuba.net>
2015-12-15 18:33:20 -0300

    bitcoin address type bug

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -27,10 +27,11 @@ import redis.clients.jedis.Jedis;
 public class BitQuest extends JavaPlugin {
     // Connecting to REDIS
     // Links to the administration account via Environment Variables
+
     public final static UUID ADMIN_UUID = System.getenv("ADMIN_UUID") != null ? UUID.fromString(System.getenv("ADMIN_UUID")) : null;
-    public final static UUID BITCOIN_ADDRESS = System.getenv("ADMIN_BITCOIN_ADDRESS") != null ? UUID.fromString(System.getenv("ADMIN_BITCOIN_ADDRESS")) : null;
-    public final static UUID BITCOIN_PRIVATE_KEY = System.getenv("ADMIN_BITCOIN_PRIVATE_KEY") != null ? UUID.fromString(System.getenv("ADMIN_BITCOIN_PRIVATE_KEY")) : null;
-    public final static UUID BLOCKCYPHER_API_KEY = System.getenv("BLOCKCYPHER_API_KEY") != null ? UUID.fromString(System.getenv("BLOCKCYPHER_API_KEY")) : null;
+    public final static String BITCOIN_ADDRESS = System.getenv("ADMIN_BITCOIN_ADDRESS") != null ? UUID.fromString(System.getenv("ADMIN_BITCOIN_ADDRESS")) : null;
+    public final static String BITCOIN_PRIVATE_KEY = System.getenv("ADMIN_BITCOIN_PRIVATE_KEY") != null ? UUID.fromString(System.getenv("ADMIN_BITCOIN_PRIVATE_KEY")) : null;
+    public final static String BLOCKCYPHER_API_KEY = System.getenv("BLOCKCYPHER_API_KEY") != null ? UUID.fromString(System.getenv("BLOCKCYPHER_API_KEY")) : null;
 
     // If env MOD_OPS exists, server automatically ops moderators
     public final static String MOD_OPS = System.getenv("MOD_OPS") != null ? System.getenv("MOD_OPS") : null;
