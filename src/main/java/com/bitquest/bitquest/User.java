a70e07ad657b225d83eeb7d80345295cca59393b
explodi <phaser@tubatuba.net>
2016-05-23 01:24:13 -0400

    new experience calculation test

--- src/main/java/com/bitquest/bitquest/User.java
+++ src/main/java/com/bitquest/bitquest/User.java
@@ -48,7 +48,7 @@ public class User {
     public void addExperience(int exp) {
         BitQuest.REDIS.incrBy("experience.raw."+this.player.getUniqueId().toString(),exp);
         setTotalExperience(experience());
-
+        System.out.println(exp);
     }
     public int experience() {
         if(BitQuest.REDIS.get("experience.raw."+this.player.getUniqueId().toString())==null) {
@@ -76,67 +76,77 @@ public class User {
         player.setScoreboard(walletScoreboard);
     }
     public void setTotalExperience(int rawxp) {
-        // xp = the square root of raw exp
-        int xp = (int)Math.sqrt((double)rawxp);
-        if(xp<1) xp=1;
-       // System.out.println(xp);
-        /*
-        AUTHOR: Dev_Richard (https://www.spigotmc.org/members/dev_richard.38792/)
-        DESC: A simple and easy to use class that can get and set a player's total experience points.
-        Feel free to use this class in both public and private plugins, however if you release your
-        plugin please link to this gist publicly so that others can contribute and benefit from it.
-        */
-        //Levels 0 through 15
-        if(xp >= 0 && xp < 351) {
-            //Calculate Everything
-            int a = 1; int b = 6; int c = -xp;
-            int level = (int) (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-            int xpForLevel = (int) (Math.pow(level, 2) + (6 * level));
-            int remainder = xp - xpForLevel;
-            int experienceNeeded = (2 * level) + 7;
-            float experience = (float) remainder / (float) experienceNeeded;
-            experience = round(experience, 2);
-           // System.out.println("xpForLevel: " + xpForLevel);
-           // System.out.println(experience);
+//
+//        // xp = the square root of raw exp
+//        int xp = (int)Math.sqrt((double)rawxp);
+//        if(xp<1) xp=1;
+//       // System.out.println(xp);
+//        /*
+//        AUTHOR: Dev_Richard (https://www.spigotmc.org/members/dev_richard.38792/)
+//        DESC: A simple and easy to use class that can get and set a player's total experience points.
+//        Feel free to use this class in both public and private plugins, however if you release your
+//        plugin please link to this gist publicly so that others can contribute and benefit from it.
+//        */
+//        //Levels 0 through 15
+//        if(xp >= 0 && xp < 351) {
+//            //Calculate Everything
+//            int a = 1; int b = 6; int c = -xp;
+//            int level = (int) (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+//            int xpForLevel = (int) (Math.pow(level, 2) + (6 * level));
+//            int remainder = xp - xpForLevel;
+//            int experienceNeeded = (2 * level) + 7;
+//            float experience = (float) remainder / (float) experienceNeeded;
+//            experience = round(experience, 2);
+//           // System.out.println("xpForLevel: " + xpForLevel);
+//           // System.out.println(experience);
+//
+//            //Set Everything
+//            player.setLevel(level);
+//            player.setExp(experience);
+//            //Levels 16 through 30
+//        } else if(xp >= 352 && xp < 1507) {
+//            //Calculate Everything
+//            double a = 2.5; double b = -40.5; int c = -xp + 360;
+//            double dLevel = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+//            int level = (int) Math.floor(dLevel);
+//            int xpForLevel = (int) (2.5 * Math.pow(level, 2) - (40.5 * level) + 360);
+//            int remainder = xp - xpForLevel;
+//            int experienceNeeded = (5 * level) - 38;
+//            float experience = (float) remainder / (float) experienceNeeded;
+//            experience = round(experience, 2);
+//           // System.out.println("xpForLevel: " + xpForLevel);
+//            // System.out.println(experience);
+//
+//            //Set Everything
+//            player.setLevel(level);
+//            player.setExp(experience);
+//            //Level 31 and greater
+//        } else {
+//            //Calculate Everything
+//            double a = 4.5; double b = -162.5; int c = -xp + 2220;
+//            double dLevel = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+//            int level = (int) Math.floor(dLevel);
+//            int xpForLevel = (int) (4.5 * Math.pow(level, 2) - (162.5 * level) + 2220);
+//            int remainder = xp - xpForLevel;
+//            int experienceNeeded = (9 * level) - 158;
+//            float experience = (float) remainder / (float) experienceNeeded;
+//            experience = round(experience, 2);
+//           // System.out.println("xpForLevel: " + xpForLevel);
+//           // System.out.println(experience);
+//
+//            //Set Everything
+//            player.setLevel(level);
+//            player.setExp(experience);
+//        }
+        // player.setTotalExperience(rawxp);
+        // lower factor, experience is easier to get. you can increase to get the opposite effect
+        int factor=512;
+        int level= (int) Math.sqrt(rawxp/factor);
+        int nextlevel=(int) Math.pow(level+1,2);
 
-            //Set Everything
-            player.setLevel(level);
-            player.setExp(experience);
-            //Levels 16 through 30
-        } else if(xp >= 352 && xp < 1507) {
-            //Calculate Everything
-            double a = 2.5; double b = -40.5; int c = -xp + 360;
-            double dLevel = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-            int level = (int) Math.floor(dLevel);
-            int xpForLevel = (int) (2.5 * Math.pow(level, 2) - (40.5 * level) + 360);
-            int remainder = xp - xpForLevel;
-            int experienceNeeded = (5 * level) - 38;
-            float experience = (float) remainder / (float) experienceNeeded;
-            experience = round(experience, 2);
-           // System.out.println("xpForLevel: " + xpForLevel);
-            // System.out.println(experience);
-
-            //Set Everything
-            player.setLevel(level);
-            player.setExp(experience);
-            //Level 31 and greater
-        } else {
-            //Calculate Everything
-            double a = 4.5; double b = -162.5; int c = -xp + 2220;
-            double dLevel = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-            int level = (int) Math.floor(dLevel);
-            int xpForLevel = (int) (4.5 * Math.pow(level, 2) - (162.5 * level) + 2220);
-            int remainder = xp - xpForLevel;
-            int experienceNeeded = (9 * level) - 158;
-            float experience = (float) remainder / (float) experienceNeeded;
-            experience = round(experience, 2);
-           // System.out.println("xpForLevel: " + xpForLevel);
-           // System.out.println(experience);
-
-            //Set Everything
-            player.setLevel(level);
-            player.setExp(experience);
-        }
+        player.setLevel(level);
+        float progress=((float)rawxp/(float)factor)/(float)nextlevel;
+        player.setExp(progress);
         setPlayerMaxHealth();
     }
     private float round(float d, int decimalPlace) {
