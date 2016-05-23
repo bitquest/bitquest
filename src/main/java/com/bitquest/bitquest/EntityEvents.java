35e80e74353c8d05458949f93c7e6b12548775f9
PsiLupan <wcarter312@gmail.com>
2016-05-23 13:45:56 -0500

    Comment out println for status code.

--- src/main/java/com/bitquest/bitquest/EntityEvents.java
+++ src/main/java/com/bitquest/bitquest/EntityEvents.java
@@ -211,10 +211,11 @@ public class EntityEvents implements Listener {
             DataOutputStream wr = new DataOutputStream(con.getOutputStream());
             wr.flush();
             wr.close();
-            int responseCode = con.getResponseCode();
-            // System.out.println("\nSending 'POST' request to URL : " + url);
-            // System.out.println("Post parameters : " + urlParameters);
-            System.out.println("Response Code : " + responseCode);
+            
+            /*final int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Post parameters : " + urlParameters);
+            System.out.println("Response Code : " + responseCode);*/
 
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
