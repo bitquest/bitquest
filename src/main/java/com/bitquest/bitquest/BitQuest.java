e902f083dbf4ed19a0f7e22e8b48b3588a61f80b
explodi <phaser@tubatuba.net>
2016-11-21 23:24:30 -0300

    getTestnetCoins feedback

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -696,7 +696,11 @@ public class  BitQuest extends JavaPlugin {
                 User user= null;
                 try {
                     user = new User(player);
-                    user.wallet.getTestnetCoins();
+                    if(user.wallet.getTestnetCoins()==true) {
+                        player.sendMessage(ChatColor.GREEN+"Some testnet coins were delivered to your wallet.");
+                    } else {
+                        player.sendMessage(ChatColor.RED+"There was an error getting testnet coins.");
+                    }
 
                 } catch (ParseException e) {
                     e.printStackTrace();
