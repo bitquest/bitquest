4771fbcf4584e3c9794d3bf2f81b967174b2dede
Juan Jose <juanjmeneses@gmail.com>
2017-07-22 15:54:42 -0500

    Move commands to its own class

---
+++ src/main/java/com/bitquest/bitquest/commands/SendCommand.java
@@ -0,0 +1,81 @@
+package com.bitquest.bitquest.commands;
+
+import com.bitquest.bitquest.BitQuest;
+import com.bitquest.bitquest.User;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+
+public class SendCommand extends CommandAction {
+    private BitQuest bitQuest;
+
+    public SendCommand(BitQuest plugin) {
+        bitQuest = plugin;
+    }
+
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args, Player player) {
+        /***********************************************************
+         /send
+         a player-to-player-transaction
+         ***********************************************************/
+        if(args.length==2) {
+            for(char c : args[0].toCharArray()) {
+                if(!Character.isDigit(c))
+                    return false;
+            }
+            int bits=Integer.valueOf(args[0]);
+            if(bits>0&&bits<=10000) {
+                int sat=bits*100;
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    if(onlinePlayer.getName().equalsIgnoreCase(args[1])) {
+                        if (!args[1].equalsIgnoreCase(player.getDisplayName())) {
+
+                            try {
+                                User user=new User(player);
+                                User user_tip=new User(onlinePlayer);
+                                if(user.wallet.payment(sat,user_tip.wallet.address)==true) {
+                                    bitQuest.updateScoreboard(onlinePlayer);
+                                    bitQuest.updateScoreboard(player);
+                                    player.sendMessage(ChatColor.GREEN+"You sent "+bits+" bits to user "+onlinePlayer.getName());
+                                    onlinePlayer.sendMessage(ChatColor.GREEN+"You got "+bits+" bits from user "+player.getName());
+                                    return true;
+                                } else {
+                                    player.sendMessage(ChatColor.RED+"Tip failed.");
+                                    return true;
+                                }
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                                player.sendMessage(ChatColor.RED+"Tip failed.");
+                                return true;
+                            } catch (org.json.simple.parser.ParseException e) {
+                                e.printStackTrace();
+                                player.sendMessage(ChatColor.RED+"Tip failed.");
+                                return true;
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                player.sendMessage(ChatColor.RED+"Tip failed.");
+                                return true;
+                            }
+                        } else {
+                            player.sendMessage(ChatColor.RED+"You cannot send to yourself!");
+                        }
+                    }
+                }
+                player.sendMessage(ChatColor.RED+"Player "+args[1]+" is not online");
+
+                return true;
+            } else {
+                player.sendMessage("Minimum tip is 1 bit. Maximum is 10000");
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/bitquest/bitquest/commands/SpectateCommand.java
@@ -0,0 +1,32 @@
+package com.bitquest.bitquest.commands;
+
+import com.bitquest.bitquest.BitQuest;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class SpectateCommand extends CommandAction {
+    private BitQuest bitQuest;
+
+    public SpectateCommand(BitQuest plugin) {
+        this.bitQuest = plugin;
+    }
+
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args, Player player) {
+        if (args.length == 1) {
+
+            if(Bukkit.getPlayer(args[0]) != null) {
+                ((Player) sender).setGameMode(GameMode.SPECTATOR);
+                ((Player) sender).setSpectatorTarget(Bukkit.getPlayer(args[0]));
+                bitQuest.success(((Player) sender), "You're now spectating " + args[0] + ".");
+            } else {
+                bitQuest.error(((Player) sender), "Player " + args[0] + " isn't online.");
+            }
+            return true;
+        }
+        return false;
+    }
+}
