2e055c0bdd7cfafb6742a5cb4ec5a3fe1ce981ad
explodi <phaser@tubatuba.net>
2017-09-27 15:25:23 -0300

    fixes BITCOIN_NODE_PORT static variable

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -58,7 +58,7 @@ public class  BitQuest extends JavaPlugin {
     public final static String WORLD_PRIVATE_KEY = System.getenv("WORLD_PRIVATE_KEY") != null ? System.getenv("WORLD_PRIVATE_KEY") : "76e8a7eb479256c68f59f66c7b744891bc2f632ff3c7a3f69a5c4aeccda687e3";
     public final static String WORLD_PUBLIC_KEY = System.getenv("WORLD_PUBLIC_KEY") != null ? System.getenv("WORLD_PUBLIC_KEY") : "76e8a7eb479256c68f59f66c7b744891bc2f632ff3c7a3f69a5c4aeccda687e3";
     public final static String BITCOIN_NODE_HOST = System.getenv("BITCOIN_NODE_HOST") != null ? System.getenv("BITCOIN_NODE_HOST") : "localhost";
-    public final static int BITCOIN_NODE_PORT = System.getenv("BITCOIN_NODE_PORT") != null ? System.getenv("BITCOIN_NODE_PORT") : 18332;
+    public final static int BITCOIN_NODE_PORT = System.getenv("BITCOIN_NODE_PORT") != null ? Integer.parseInt(System.getenv("BITCOIN_NODE_PORT")) : 18332;
     public final static String BITCOIN_NODE_USERNAME = System.getenv("BITCOIN_NODE_USERNAME") != null ? System.getenv("BITCOIN_NODE_USERNAME") : null;
     public final static String BITCOIN_NODE_PASSWORD = System.getenv("BITCOIN_NODE_PASSWORD") != null ? System.getenv("BITCOIN_NODE_PASSWORD") : null;
 
