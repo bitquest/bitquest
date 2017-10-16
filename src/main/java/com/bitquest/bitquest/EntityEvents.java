2db2d4ddbceb14ba2dc8907e149d6d2ec49539b6
Will C <wcarter312@gmail.com>
2017-10-15 22:23:46 -0500

    Don't need a second change because ternary operator handles the null evaluation.

--- src/main/java/com/bitquest/bitquest/EntityEvents.java
+++ src/main/java/com/bitquest/bitquest/EntityEvents.java
@@ -255,11 +255,8 @@ public class EntityEvents implements Listener {
                 int x2=event.getTo().getChunk().getX();
                 int z2=event.getTo().getChunk().getZ();
 
-                String name1=BitQuest.REDIS.get("chunk"+x1+","+z1+"name")!= null ? BitQuest.REDIS.get("chunk"+x1+","+z1+"name") : "the wilderness";
-                String name2=BitQuest.REDIS.get("chunk"+x2+","+z2+"name")!= null ? BitQuest.REDIS.get("chunk"+x2+","+z2+"name") : "the wilderness";
-
-                if(name1==null) name1="the wilderness";
-                if(name2==null) name2="the wilderness";
+                String name1 = BitQuest.REDIS.get("chunk"+x1+","+z1+"name")!= null ? BitQuest.REDIS.get("chunk"+x1+","+z1+"name") : "the wilderness";
+                String name2 = BitQuest.REDIS.get("chunk"+x2+","+z2+"name")!= null ? BitQuest.REDIS.get("chunk"+x2+","+z2+"name") : "the wilderness";
 
                 if(!name1.equals(name2)) {
                     if(name2.equals("the wilderness")){
