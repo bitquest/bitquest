f8c49eb96cc0b8e9fae8d5ae5c682d4c8ea9ba63
Jorropo <jorropo.pgm@gmail.com>
2018-05-17 06:49:08 +0200

    Code format.

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -248,7 +248,7 @@ public class BitQuest extends JavaPlugin {
             String inputLine;
             StringBuffer response = new StringBuffer();
 
-            while ((inputLine = in .readLine()) != null) {
+            while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             } in .close();
             System.out.println(response.toString());
