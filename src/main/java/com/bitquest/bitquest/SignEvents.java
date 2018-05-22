bf4932b04154f0427256db513f1eb7212e64f1b2
Jorropo <jorropo.pgm@gmail.com>
2018-05-22 11:09:53 +0200

    Code autoformat

--- src/main/java/com/bitquest/bitquest/SignEvents.java
+++ src/main/java/com/bitquest/bitquest/SignEvents.java
@@ -1,51 +1,46 @@
 package com.bitquest.bitquest;
 
-import com.mixpanel.mixpanelapi.ClientDelivery;
-import com.mixpanel.mixpanelapi.MixpanelAPI;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.World.Environment;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.SignChangeEvent;
-import org.bukkit.event.world.WorldLoadEvent;
-import org.bukkit.scheduler.BukkitScheduler;
-
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.UUID;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 
 public class SignEvents implements Listener {
-    BitQuest bitQuest;
-    public SignEvents(BitQuest plugin) {
-        bitQuest = plugin;
+  BitQuest bitQuest;
+
+  public SignEvents(BitQuest plugin) {
+    bitQuest = plugin;
+  }
+
+  @EventHandler
+  public void onSignChange(SignChangeEvent event)
+      throws ParseException, org.json.simple.parser.ParseException, IOException {
+
+    final Player player = event.getPlayer();
+    // Check that the world is overworld
+    if (!event.getBlock().getWorld().getName().endsWith("_nether")
+        && !event.getBlock().getWorld().getName().endsWith("_end")) {
+      final String specialCharacter = "^";
+      final String[] lines = event.getLines();
+      final String signText = lines[0] + lines[1] + lines[2] + lines[3];
+      Chunk chunk = event.getBlock().getWorld().getChunkAt(event.getBlock().getLocation());
+
+      if (signText.length() > 0
+          && signText.substring(0, 1).equals(specialCharacter)
+          && signText.substring(signText.length() - 1).equals(specialCharacter)) {
+
+        final String name = signText.substring(1, signText.length() - 1);
+        bitQuest.claimLand(name, chunk, player);
+      }
+
+    } else if (event.getBlock().getWorld().getName().endsWith("_nether")) {
+      player.sendMessage(ChatColor.DARK_RED + "No claiming in the nether!");
+    } else if (event.getBlock().getWorld().getName().endsWith("_end")) {
+      player.sendMessage(ChatColor.DARK_RED + "No claiming in the end!");
     }
-    @EventHandler
-    public void onSignChange(SignChangeEvent event) throws ParseException, org.json.simple.parser.ParseException, IOException {
-
-        final Player player = event.getPlayer();
-        // Check that the world is overworld
-        if (!event.getBlock().getWorld().getName().endsWith("_nether") && !event.getBlock().getWorld().getName().endsWith("_end")) {
-            final String specialCharacter = "^";
-            final String[] lines = event.getLines();
-            final String signText = lines[0] + lines[1] + lines[2] + lines[3];
-            Chunk chunk = event.getBlock().getWorld().getChunkAt(event.getBlock().getLocation());
-
-            if (signText.length() > 0 && signText.substring(0, 1).equals(specialCharacter) && signText.substring(signText.length() - 1).equals(specialCharacter)) {
-
-                final String name = signText.substring(1, signText.length() - 1);
-                bitQuest.claimLand(name, chunk, player);
-
-            }
-
-        } else if (event.getBlock().getWorld().getName().endsWith("_nether")) {
-            player.sendMessage(ChatColor.DARK_RED + "No claiming in the nether!");
-        } else if (event.getBlock().getWorld().getName().endsWith("_end")) {
-            player.sendMessage(ChatColor.DARK_RED + "No claiming in the end!");
-        }
-
-    }
+  }
 }
--- src/main/java/com/bitquest/bitquest/commands/EmergencystopCommand.java
+++ src/main/java/com/bitquest/bitquest/commands/EmergencystopCommand.java
@@ -6,22 +6,22 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class EmergencystopCommand extends CommandAction {
-    public boolean run(CommandSender sender, Command cmd, String label, String[] args, Player player) {
-        StringBuilder message = new StringBuilder();
-        message.append(sender.getName())
-            .append(" has shut down the server for emergency reasons");
+  public boolean run(
+      CommandSender sender, Command cmd, String label, String[] args, Player player) {
+    StringBuilder message = new StringBuilder();
+    message.append(sender.getName()).append(" has shut down the server for emergency reasons");
 
-        if (args.length > 0) {
-            message.append(": ");
-            for (String word: args) {
-                message.append(word).append(" ");
-            }
-        }
-        for (Player currentPlayer: Bukkit.getOnlinePlayers()) {
-            currentPlayer.kickPlayer(message.toString());
-        }
-
-        Bukkit.shutdown();
-        return true;
+    if (args.length > 0) {
+      message.append(": ");
+      for (String word : args) {
+        message.append(word).append(" ");
+      }
     }
+    for (Player currentPlayer : Bukkit.getOnlinePlayers()) {
+      currentPlayer.kickPlayer(message.toString());
+    }
+
+    Bukkit.shutdown();
+    return true;
+  }
 }
