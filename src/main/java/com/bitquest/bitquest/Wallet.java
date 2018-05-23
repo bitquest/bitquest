b03290d5872af9160b4adb5e8aa32c67fac2f99d
Jorropo <jorropo.pgm@gmail.com>
2018-05-23 13:08:31 +0200

    Fixing import

--- src/main/java/com/bitquest/bitquest/Wallet.java
+++ src/main/java/com/bitquest/bitquest/Wallet.java
@@ -7,7 +7,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import java.util.Base64.Encoder;
+import java.util.Base64;
 
 public class Wallet {
   public int balance;
