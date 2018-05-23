8c3a40d70de30c857741844f84066d1fe7f5dcca
AltQuest <altquest@gmail.com>
2018-05-23 12:04:58 +0200

    Added /fixabandonland Work by AltQuest, porting to last version by Jorropo (it were very simple). Tested by recreating the issue locally. would seem to work 100% you can run just /fixabandonland to see list of info, then run /fixabandonland [true|yes] to remove what it stated before would be removed.

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -246,6 +246,7 @@ public class BitQuest extends JavaPlugin {
     modCommands.put("banlist", new BanlistCommand());
     modCommands.put("spectate", new SpectateCommand(this));
     modCommands.put("emergencystop", new EmergencystopCommand());
+    modCommands.put("fixabandonland", new FixAbandonLand());
     // TODO: Remove this command after migrate.
     modCommands.put("migrateclans", new MigrateClansCommand());
     sendDiscordMessage("bitquest started");
