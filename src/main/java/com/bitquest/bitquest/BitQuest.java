d5ab74aa1eee299be956095143361b2d0a7d8fe3
explodi <phaser@tubatuba.net>
2018-08-13 08:49:26 -0300

    db url fix

--- src/main/java/com/bitquest/bitquest/BitQuest.java
+++ src/main/java/com/bitquest/bitquest/BitQuest.java
@@ -145,7 +145,7 @@ public class BitQuest extends JavaPlugin {
     private Map<String, CommandAction> modCommands;
     private Player[] moderators;
     public static long PET_PRICE = 100 * DENOMINATION_FACTOR;
-    public static final String db_url = "jdbc:postgresql://" + System.getenv("POSTGRES_1_PORT_5432_TCP_ADDR") + ":" + System.getenv("POSTGRES_1_PORT_5432_PORT") + "/bitquest";
+    public static final String db_url = "jdbc:postgresql://" + System.getenv("POSTGRES_1_PORT_5432_TCP_ADDR") + ":" + System.getenv("POSTGRES_1_PORT_5432_TCP_PORT") + "/bitquest";
     public static final String db_user = System.getenv("BITQUEST_POSTGRESQL_USERNAME");
     public static final String db_password = System.getenv("BITQUEST_POSTGRESQL_PASSWORD");
     public java.sql.Connection db_con;
