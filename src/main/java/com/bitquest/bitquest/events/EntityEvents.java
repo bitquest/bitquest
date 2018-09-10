48fdbf37fe4bcb0d30c7dd1850d57cd8607f2913
explodi <phaser@tubatuba.net>
2018-09-10 00:30:33 -0300

    shutdown on login event fail

--- src/main/java/com/bitquest/bitquest/events/EntityEvents.java
+++ src/main/java/com/bitquest/bitquest/events/EntityEvents.java
@@ -116,23 +116,28 @@ public class EntityEvents implements Listener {
 
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
-        Player player = event.getPlayer();
+        try {
+            Player player = event.getPlayer();
 
-        if (!(BitQuest.REDIS.exists("name:" + player.getUniqueId().toString()))) {
-            //give new players a compass @BitcoinJake09
-            player.getInventory().addItem(new ItemStack(Material.COMPASS, 1));
-        }
-        //sets currency flag if not set. @BitcoinJake09
-        if (BitQuest.REDIS.get("currency" + player.getUniqueId().toString()) == null) {
-            BitQuest.REDIS.set("currency" + player.getUniqueId().toString(), BitQuest.DENOMINATION_NAME);
-        } else if ((BitQuest.REDIS.get("currency" + player.getUniqueId().toString()) == null)) {
-            BitQuest.REDIS.set("currency" + player.getUniqueId().toString(), "emerald");
-        }
+            if (!(BitQuest.REDIS.exists("name:" + player.getUniqueId().toString()))) {
+                //give new players a compass @BitcoinJake09
+                player.getInventory().addItem(new ItemStack(Material.COMPASS, 1));
+            }
+            //sets currency flag if not set. @BitcoinJake09
+            if (BitQuest.REDIS.get("currency" + player.getUniqueId().toString()) == null) {
+                BitQuest.REDIS.set("currency" + player.getUniqueId().toString(), BitQuest.DENOMINATION_NAME);
+            } else if ((BitQuest.REDIS.get("currency" + player.getUniqueId().toString()) == null)) {
+                BitQuest.REDIS.set("currency" + player.getUniqueId().toString(), "emerald");
+            }
 
-        BitQuest.REDIS.set("name:" + player.getUniqueId().toString(), player.getName());
-        BitQuest.REDIS.set("uuid:" + player.getName().toString(), player.getUniqueId().toString());
-        if (BitQuest.REDIS.sismember("banlist", event.getPlayer().getUniqueId().toString())) {
-            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, PROBLEM_MESSAGE);
+            BitQuest.REDIS.set("name:" + player.getUniqueId().toString(), player.getName());
+            BitQuest.REDIS.set("uuid:" + player.getName().toString(), player.getUniqueId().toString());
+            if (BitQuest.REDIS.sismember("banlist", event.getPlayer().getUniqueId().toString())) {
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, PROBLEM_MESSAGE);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            Bukkit.shutdown();
         }
     }
 
