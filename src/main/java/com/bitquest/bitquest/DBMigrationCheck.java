31c3f90497b38b9850641f4f35ead3d2c4656cfd
cristian <cristian@ginzu2000.mego.games>
2018-09-10 16:50:13 -0300

    new users schema

--- src/main/java/com/bitquest/bitquest/DBMigrationCheck.java
+++ src/main/java/com/bitquest/bitquest/DBMigrationCheck.java
@@ -6,7 +6,7 @@ public class DBMigrationCheck {
 
     public DBMigrationCheck(Connection con) throws SQLException {
         System.out.println("[db] checking for table 'users'");
-        PreparedStatement ps = con.prepareStatement("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, uuid VARCHAR(128) UNIQUE, private VARCHAR(256), public VARCHAR(256), address VARCHAR(128), wif VARCHAR(256), experience INTEGER DEFAULT 0)");
+        PreparedStatement ps = con.prepareStatement("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, uuid VARCHAR(128) UNIQUE, private VARCHAR(256), public VARCHAR(256), address VARCHAR(128), wif VARCHAR(256), experience INTEGER DEFAULT 0, last_seen TIMESTAMP)");
         ps.executeUpdate();
     }
 }
