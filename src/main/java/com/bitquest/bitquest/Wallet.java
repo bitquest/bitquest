a4873ad38fd9dc03caa2c1d71d5b3b0846ff0bba
explodi <phaser@tubatuba.net>
2018-12-10 10:49:33 -0300

    balances for bitcoin and bitcoin testnet fixed

--- src/main/java/com/bitquest/bitquest/Wallet.java
+++ src/main/java/com/bitquest/bitquest/Wallet.java
@@ -218,7 +218,8 @@ public class Wallet {
   }
   public Long getBalance(int confirmations) throws IOException, ParseException {
     HttpsURLConnection c = null;
-    URL u = new URL("https://api.blockcypher.com/v1/" + BitQuest.BLOCKCYPHER_CHAIN + "/addrs/" + this.address + "/balance");
+    URL u=new URL("https://blockchain.info/q/addressbalance/" + this.address);
+    if(BitQuest.BLOCKCYPHER_CHAIN.equals("btc/test3")) u = new URL("https://testnet.blockchain.info/q/addressbalance/" + this.address+"?confirmations="+confirmations);
     c = (HttpsURLConnection) u.openConnection();
     c.setRequestMethod("GET");
     c.setRequestProperty("Content-length", "0");
@@ -228,6 +229,9 @@ public class Wallet {
     int status = c.getResponseCode();
     System.out.println("[balance] status:"+status);
     switch (status) {
+      case 500:
+        System.out.println(u.toString());
+
       case 200:
       case 201:
         BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
@@ -237,10 +241,12 @@ public class Wallet {
           sb.append(line + "\n");
         }
         br.close();
-        System.out.println(sb.toString());
-        JSONParser parser = new JSONParser();
-        JSONObject response_object = (JSONObject) parser.parse(sb.toString());
-        return (Long) response_object.get("final_balance");
+        System.out.println("[balance] "+this.address+": "+sb.toString().trim());
+        return Long.parseLong(sb.toString().trim());
+        // JSONParser parser = new JSONParser();
+
+        // JSONObject response_object = (JSONObject) parser.parse(sb.toString());
+        // return (Long) response_object.get("final_balance");
       case 429:
         throw new IOException("Rate limit");
     }
